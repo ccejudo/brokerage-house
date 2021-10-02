239135f4c8d7d7c2b415f87b5161fa15ffe29c0e
ccejudo <cristopher.cm1999@gmail.com>
2021-10-01 20:22:07 -0500

    Add Menu Avatar and Options

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -1,15 +1,44 @@
 import React, { useContext } from 'react'
-import { Grid, Avatar } from '@mui/material'
+import { Grid, Avatar, List, ListItem, ListItemButton, ListItemText, ListItemAvatar, Divider } from '@mui/material'
 import AppContext from '../AppContex'
 
 function Menu() {
     const context = useContext(AppContext)
+    const menu_options = ["Front Office", "Middle Office", "Back Office"]
+
+    const renderMenuOptions = ( options ) => {
+        return options.map( option => {
+            return(
+                <>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemText
+                                primary={<h3 style={styles.menuOption}>{option}</h3>}
+                            />
+                        </ListItemButton>
+                    </ListItem>
+                    <Divider component="li" sx={{backgroundColor:"#FC8E61"}}/>
+                </>
+            )
+        })
+    }
 
     return (
         <Grid container item xs={3} style={styles.menu}>
             <Grid item xs={12}>
-                <Avatar alt="Avatar Image" src="/images/"/>
-                <p>{context.user.name}</p>
+                <List>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar alt="Avatar Image" src={context.user.avatar} sx={{ width: 56, height: 56 }}/>
+                        </ListItemAvatar>
+                        <ListItemText
+                            sx={styles.avatarText}
+                            primary = {<h2>{context.user.name}</h2>}
+                        />
+                    </ListItem>
+                    <Divider component="li" sx={{backgroundColor:"#FC8E61"}}/>
+                    { renderMenuOptions(menu_options) }
+                </List>
             </Grid>
         </Grid>
     )
@@ -17,8 +46,17 @@ function Menu() {
 
 const styles = {
     menu: {
+        padding: '2% 1%',
         backgroundColor: '#063B56',
-        color: 'white'
+        color: 'white',
+    },
+
+    avatarText: {
+        margin: '0 1rem',
+    },
+
+    menuOption: {
+        textAlign: 'center',
     }
 }
 
